5395c98743ac4849a7f95f1df533327aba72fc36
Alexle27 <42551905+Alexle27@users.noreply.github.com>
2019-01-17 08:23:15 -0600

    Add files via upload

---
+++ hello-master/hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: David Wacker
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("David Wacker\n");
+  printf("Mechanical Engineering");
+  printf("Alex Le\n");
+  printf("Computer Engineering");
+
+  return 0;
+}
